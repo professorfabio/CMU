9893e2486cbce83469d82b697d27126725f2ad79
professorfabio <fmc@ufg.br>
2022-02-23 22:36:11 -0300

    Add first version of temperature server, which Kafka-consumes temperature and serves via gRPC

--- grpc/python/thermometer-kafka-consumer.py
+++ grpc/python/thermometer-kafka-consumer.py
@@ -1,8 +1,37 @@
 from kafka import KafkaConsumer
+import thread
+
+from concurrent import futures
+import logging
+
+import grpc
+import temperature_service_pb2
+import temperature_service_pb2_grpc
+
+current_temperature = ''
+
+// Kafka consumer to run on a separate thread
+def consume_temperature():
+    consumer = KafkaConsumer(bootstrap_servers='34.133.59.232:9092')
+    consumer.subscribe(topics=('temperature'))
+    for msg in consumer:
+        print (msg.value.decode())
+        current_temperature = msg.value.decode()
+
+class TemperatureServer(temperature_service_pb2_grpc.TemperatureServiceServicer):
+
+    def SayTemperature(self, request, context):
+        return temperature_service_pb2.TemperatureReply(temperature=current_temperature)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    temperature_service_pb2_grpc.add_TemperatureServiceServicer_to_server(TemperatureService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
 
 
-
-consumer = KafkaConsumer(bootstrap_servers='34.133.59.232:9092')
-consumer.subscribe(topics=('temperature'))
-for msg in consumer:
-    print (msg.value.decode())
+if __name__ == '__main__':
+    logging.basicConfig()
+    thread.start_new_thread(consume_temperature,())
+    serve()
