9f6a478e38c72519cd93fe28ff785f3e73b2d4c1
professorfabio <fmc@ufg.br>
2022-02-23 21:45:21 -0300

    Change to kafka consumer to print only the temperature value (not the entire message)

--- sensors/thermometer-kafka-consumer.py
+++ sensors/thermometer-kafka-consumer.py
@@ -3,4 +3,4 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer(bootstrap_servers='34.133.59.232:9092')
 consumer.subscribe(topics=('temperature'))
 for msg in consumer:
-    print (msg)
+    print (msg[value])
