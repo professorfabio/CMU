00f5e8fc35afd3d7ac2b52599e76ac854f1256e2
Fábio M. Costa <fmc@ufg.br>
2022-02-24 18:10:56 -0300

    Update led_blinking.py

--- sensors/led_blinking.py
+++ sensors/led_blinking.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 
 while True: # Run forever
-	GPIO.output(8, GPIO.HIGH) # Turn on
+	GPIO.output(16, GPIO.HIGH) # Turn on
 	sleep(1) # Sleep for 1 second
-	GPIO.output(8, GPIO.LOW) # Turn off
+	GPIO.output(16, GPIO.LOW) # Turn off
 	sleep(1) # Sleep for 1 second
