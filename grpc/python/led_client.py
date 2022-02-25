bfcba4315ae5844fef475b71e8bb8b2e213f9e5c
professorfabio <fmc@ufg.br>
2022-02-24 21:08:41 -0300

    tst

--- grpc/python/led_client.py
+++ grpc/python/led_client.py
@@ -31,7 +31,7 @@ def run():
         stub = temperature_service_pb2_grpc.TemperatureServiceStub(channel)
         response = stub.BlinkLed(temperature_service_pb2.LedMessage(state=int(sys.argv[1])))
 
-    print("Led state: " + response.state)
+    print("Led state: ", response.state)
 
 if __name__ == '__main__':
     logging.basicConfig()
