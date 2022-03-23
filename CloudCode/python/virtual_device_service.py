4664362ff7e3f6dde7a5ce8070423240400e589e
GabrielVD <g.viana.dantas@gmail.com>
2022-03-23 18:02:45 -0300

    Virtual device service access control

--- CloudCode/python/virtual_device_service.py
+++ CloudCode/python/virtual_device_service.py
@@ -3,6 +3,7 @@ import threading
 
 from concurrent import futures
 import logging
+import secrets
 
 import grpc
 import iot_service_pb2
@@ -11,6 +12,9 @@ import iot_service_pb2_grpc
 # Twin state
 current_temperature = 'void'
 led_state = {'red':0, 'green':0}
+serial_keys = {} # key : devices
+sessions = {} # session : user
+users = {} # user : {password, devices}
 
 # Kafka consumer to run on a separate thread
 def consume_temperature():
@@ -32,12 +36,53 @@ class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
         return iot_service_pb2.TemperatureReply(temperature=current_temperature)
     
     def BlinkLed(self, request, context):
-        print ("Blink led ", request.ledname)
-        print ("...with state ", request.state)
-        produce_led_command(request.state, request.ledname)
-        # Update led state of twin
-        led_state[request.ledname] = request.state
-        return iot_service_pb2.LedReply(ledstate=led_state)
+        state = {}
+        if request.session not in sessions:
+            print('Session ', request.session, ' rejected')
+        elif request.ledname not in users[sessions[request.session]].devices:
+            print('Session not authorized to access device')
+        else:
+            print ("Blink led ", request.ledname)
+            print ("...with state ", request.state)
+            produce_led_command(request.state, request.ledname)
+            # Update led state of twin
+            led_state[request.ledname] = request.state
+            state = led_state
+        return iot_service_pb2.LedReply(ledstate=state)
+    
+    def RegisterDevices(self, request, context):
+        print('Registering devices ', request.device_keys, ' under serial key ', request.serial_key)
+        serial_keys[request.serial_key] = request.device_keys
+        return iot_service_pb2.RegisterReply()
+    
+    def Login(self, request, context):
+        if request.user not in users:
+            users[request.user] = {
+                'password': request.password,
+                'devices': set()
+            }
+
+        session = -1
+        if users[request.user].password == request.password:
+            session = secrets.randbits(32)
+            sessions[session] = request.user
+
+        print('Login from user ', request.user, ' returned session ', session)
+        return iot_service_pb2.LoginReply(session=session)
+
+    def RegisterKey(self, request, context):
+        devices = set()
+        if request.session in sessions and request.serial_key in serial_keys:
+            user = sessions[request.session]
+            devices = serial_keys[request.serial_key]
+            users[user].devices.update(devices)
+            print('User ', user, ' is authorized to access devices ', devices)
+        else:
+            print('Authorization rejected for session ', request.session, ' and key ', request.serial_key)
+
+        return iot_service_pb2.KeyReply(device_keys=devices)
+
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
