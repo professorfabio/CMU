1606311838eff06ebd7585f2ab512e0b0329729c
GabrielVD <g.viana.dantas@gmail.com>
2022-03-31 22:26:41 -0300

    SSO service

--- CloudCode/python/virtual_device_service.py
+++ CloudCode/python/virtual_device_service.py
@@ -14,7 +14,7 @@ current_temperature = 'void'
 led_state = {'red':0, 'green':0}
 serial_keys = {} # key : devices
 sessions = {} # session : user
-users = {} # user : {password, devices}
+users = {} # user : {password, serial_keys}
 
 # Kafka consumer to run on a separate thread
 def consume_temperature():
@@ -25,9 +25,9 @@ def consume_temperature():
         print (msg.value.decode())
         current_temperature = msg.value.decode()
 
-def produce_led_command(state, ledname):
+def produce_led_command(state, ledname, session):
     producer = KafkaProducer(bootstrap_servers='35.226.115.184:9092')
-    producer.send('ledcommand', key=ledname.encode(), value=str(state).encode())
+    producer.send('ledcommand', key=ledname.encode(), value=str(state).encode(), sid=str(session).encode())
     return state
         
 class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
@@ -38,28 +38,26 @@ class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
     def BlinkLed(self, request, context):
         state = {}
         if request.session not in sessions:
-            print('Session ', request.session, ' rejected')
-        elif request.ledname not in users[sessions[request.session]]['devices']:
-            print('Session not authorized to access device')
+            print('Session', request.session, 'rejected')
         else:
             print ("Blink led ", request.ledname)
             print ("...with state ", request.state)
-            produce_led_command(request.state, request.ledname)
+            produce_led_command(request.state, request.ledname, request.session)
             # Update led state of twin
             led_state[request.ledname] = request.state
             state = led_state
         return iot_service_pb2.LedReply(ledstate=state)
     
     def RegisterDevices(self, request, context):
-        print('Registering devices ', request.device_keys, ' under serial key ', request.serial_key)
+        print('Registering devices', request.device_keys, 'under serial key', request.serial_key)
         serial_keys[request.serial_key] = request.device_keys
-        return iot_service_pb2.RegisterReply()
+        return iot_service_pb2.DeviceReply()
     
     def Login(self, request, context):
         if request.user not in users:
             users[request.user] = {
                 'password': request.password,
-                'devices': set()
+                'serial_keys': set()
             }
 
         session = 0
@@ -67,7 +65,7 @@ class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
             session = secrets.randbits(32)
             sessions[session] = request.user
 
-        print('Login from user ', request.user, ' returned session ', session)
+        print('Login from user', request.user, 'returned session', session)
         return iot_service_pb2.LoginReply(session=session)
 
     def RegisterKey(self, request, context):
@@ -75,12 +73,19 @@ class IoTServer(iot_service_pb2_grpc.IoTServiceServicer):
         if request.session in sessions and request.serial_key in serial_keys:
             user = sessions[request.session]
             devices = serial_keys[request.serial_key]
-            users[user]['devices'].update(devices)
-            print('User ', user, ' is authorized to access devices ', devices)
+            users[user]['serial_keys'].add(request.serial_key)
+            print('User', user, 'acquired serial key', request.serial_key)
         else:
-            print('Authorization rejected for session ', request.session, ' and key ', request.serial_key)
+            print('Key registration rejected for session', request.session, 'and key', request.serial_key)
 
         return iot_service_pb2.KeyReply(device_keys=devices)
+
+    def Authorize(self, request, context):
+        authorized = request.session in sessions and request.serial_key in users[sessions[request.session]]['serial_keys']
+        print('Authorization for session', request.session, 'to serial key', request.serial_key, 'is', authorized)
+
+        return iot_service_pb2.AuthorizeReply(authorized=authorized)
+
 
 
 
