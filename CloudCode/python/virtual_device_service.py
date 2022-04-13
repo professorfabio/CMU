c08b093a7c466d44e20791b70db2a40617d87279
GabrielVD <g.viana.dantas@gmail.com>
2022-04-13 08:56:10 -0300

    Attribute morse

--- CloudCode/python/virtual_device_service.py
+++ CloudCode/python/virtual_device_service.py
@@ -15,7 +15,7 @@ import id_provider_pb2_grpc
 current_temperature = 'void'
 sessions = {} # session -> {user, roles}
 device_attributes = {} # device -> attributes
-roles = {
+role_attributes = {
     'teacher': {'temperature', 'led_red', 'led_green'},
     'student': {'temperature', 'led_green'}
 } # role -> attributes
@@ -49,6 +49,9 @@ def call_attribute(environment, attribute, parameter):
         if attribute == 'temperature':
             return current_temperature
 
+        if attribute == 'morse':
+            return morse(parameter)
+            
         # led
         print ("Blink led ", attribute)
         print ("...with state ", parameter)
@@ -81,7 +84,7 @@ def validate_AttributeRequest(request):
         return False
     
     for role in credentials['roles']:
-        if request.attribute in roles[role]:
+        if request.attribute in role_attributes[role]:
             return True
     
     print('User', credentials['user'], 'not authorized to access attribute', request.attribute)
@@ -115,3 +118,50 @@ if __name__ == '__main__':
     trd = threading.Thread(target=consume_temperature)
     trd.start()
     serve()
+
+morse_table = {
+    'a': '. _',
+    'b': '_ . . .',
+    'c': '_ . _ .',
+    'd': '_ . .',
+    'e': '.',
+    'f': '. . _ .',
+    'g': '_ _ .',
+    'h': '. . . .',
+    'i': '. .',
+    'j': '. _ _ _',
+    'k': '_ . _',
+    'l': '. _ . .',
+    'm': '_ _',
+    'n': '_ .',
+    'o': '_ _ _',
+    'p': '. _ _ .',
+    'q': '_ _ . _',
+    'r': '. _ .',
+    's': '. . .',
+    't': '_',
+    'u': '. . _',
+    'v': '. . . _',
+    'w': '. _ _',
+    'x': '_ . . _',
+    'y': '_ . _ _',
+    'z': '_ _ . .',
+    '1': '. _ _ _ _',
+    '2': '. . _ _ _',
+    '3': '. . . _ _',
+    '4': '. . . . _',
+    '5': '. . . . .',
+    '6': '_ . . . .',
+    '7': '_ _ . . .',
+    '8': '_ _ _ . .',
+    '9': '_ _ _ _ .',
+    '0': '_ _ _ _ _',
+    ' ': '    '
+}
+
+def morse(text):
+    morse = ''
+    for c in ' '.join(text.lower().split()):
+        if c in morse_table:
+            morse += morse_table[c] + '   '
+    return morse
